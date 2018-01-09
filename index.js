5a1be708075a13bd4ceb65314e0523f75d905944
First Last <flatironcamp.tc99@mamber.net>
2018-01-09 04:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musiciansArray, instrumentsArray) {
   var whoPlaysWhat = [];
   
   for (var i = 0; i < musiciansArray.length; i++) {
-    whoPlaysWhat.push('${musiciansArray[i]} plays ${instrumentsArray[i]');
+    whoPlaysWhat.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`);
   }
   return whoPlaysWhat;
 }
